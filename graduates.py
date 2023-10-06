4dac463fb0e74c99a1213e4e9109ed6a68886224
Maria Vittoria Passarello <86560494+mariavittoriapassarello@users.noreply.github.com>
2023-10-06 15:22:34 +0200

    Delete graduates.py

--- graduates.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd 
-graduates = ({'name': ["Maria Vittoria", "Greta", "Andrea"], 'age': [24,25,23]})
-dataframe = pd.DataFrame(graduates)
-print("Graduates:")
-print(dataframe)
-
-print("In ordine crescente di età:")
-print(dataframe.sort_values('age'))
